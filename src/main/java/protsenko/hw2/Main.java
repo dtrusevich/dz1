c3625a4705aeda1ca6a12446258f3c626cff2819
Digriz87 <fiveadvert@gmail.com>
2022-01-05 17:29:32 +0200

    hw2 VP

---
+++ src/main/java/protsenko/hw2/Main.java
@@ -0,0 +1,18 @@
+package protsenko.hw2;
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        MyList<Object> mist = new LinkedListTwo<>();
+        mist.add("first");
+        mist.add("second");
+        mist.add("three");
+
+
+
+
+    }
+}
