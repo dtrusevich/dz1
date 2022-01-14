de88a7a56b098162f08b7b4e8949a68d770448d6
Denis Trusevich <den.trusevich@gmail.com>
2022-01-14 20:54:15 +0300

    добавлен паттерн декоратор

---
+++ src/main/java/trusevich/decorators/FileDataSource.java
@@ -0,0 +1,35 @@
+package trusevich.decorators;
+
+import java.io.*;
+
+public class FileDataSource implements DataSource {
+    private final String name;
+
+    public FileDataSource(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void writeData(String data) {
+        File file = new File(name);
+        try (OutputStream fos = new FileOutputStream(file)) {
+            fos.write(data.getBytes(), 0, data.length());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @Override
+    public String readData() {
+        char[] buffer = null;
+        File file = new File(name);
+        try (FileReader ignored = new FileReader(file)) {
+            buffer = new char[(int) file.length()];
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        assert buffer != null;
+        return new String(buffer);
+    }
+}
+
